aa0bf42c3be607f6d4f0f249c40badde274b2578
SwetlanaAng <93288183+SwetlanaAng@users.noreply.github.com>
2024-09-16 11:52:52 +0200

    init: solve task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,25 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+        this.number = 0;
+        this.min= 0;
+        this.max = 0;
+    }
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
-
     guess() {
-
+        this.number = Math.round((this.min + this.max)/2)
+        return this.number;
     }
-
     lower() {
-
+        this.max = this.number;
+        this.setRange(this.min, this.max)
     }
-
     greater() {
-
+        this.min = this.number;
+        this.setRange(this.min, this.max)
     }
-}
+  }
 
 module.exports = GuessingGame;
